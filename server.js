245dec2763af033301575290bd4027876ec36e6c
Paulo Correa <pauloyaco@gmail.com>
2020-05-13 01:06:19 -0300

    express

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/pessoal-ng'));
 
 app.get('/*', function(req,res) {
-    
+
 res.sendFile(path.join(__dirname+'/dist/pessoal-ng/index.html'));
 });
 
